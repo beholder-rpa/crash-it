cf29900d8eafb0153ac16961d00869703f2ee4e2
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-02-12 04:41:29 -0500

    Fix urls, maybe

--- src/data/navigation.tsx
+++ src/data/navigation.tsx
@@ -26,23 +26,23 @@ const navigation: Navigation[] = [
     current: true,
     defaultOpen: true,
     url: '/',
-    children: [{ title: `Run 'n Gun Test`, icon: <FontAwesomeIcon icon={faPersonRunning} />, url: '/run_n_gun' }],
+    children: [{ title: `Run 'n Gun Test`, icon: <FontAwesomeIcon icon={faPersonRunning} />, url: './run_n_gun' }],
   },
   {
     title: 'Sprite Editor',
     icon: <FontAwesomeIcon icon={faGrid5} />,
     current: true,
-    url: '/sprite_editor',
+    url: './sprite_editor',
   },
-  { title: 'Tile Editor', icon: <FontAwesomeIcon icon={faGameBoard} />, current: true, url: '/tile_editor' },
+  { title: 'Tile Editor', icon: <FontAwesomeIcon icon={faGameBoard} />, current: true, url: './tile_editor' },
   {
     title: 'Character Editor',
     icon: <FontAwesomeIcon icon={faSnowman} />,
     current: true,
-    url: '/character_editor',
+    url: './character_editor',
   },
-  { title: 'Mob Editor', icon: <FontAwesomeIcon icon={faAlienMonster} />, current: true, url: '/mob_editor' },
-  { title: 'Sound Manager', icon: <FontAwesomeIcon icon={faFolderMusic} />, current: true, url: '/sound_manager' },
+  { title: 'Mob Editor', icon: <FontAwesomeIcon icon={faAlienMonster} />, current: true, url: './mob_editor' },
+  { title: 'Sound Manager', icon: <FontAwesomeIcon icon={faFolderMusic} />, current: true, url: './sound_manager' },
 ];
 
 export default navigation;
